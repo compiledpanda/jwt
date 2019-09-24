3d89822f2e3f16754e76abe4639428a891a72d27
John Clark <socrates37@gmail.com>
2019-09-24 06:49:19 -0600

    test existing code

--- jwt_test.go
+++ jwt_test.go
@@ -8,10 +8,64 @@ import (
 )
 
 func TestRoot(t *testing.T) {
-	cmd := exec.Command("go", "run", "jwt.go")
-	output, err := cmd.Output()
+	type test struct {
+		args []string
+	}
 
-	// Should have some output and not errored
-	assert.Nil(t, err)
-	assert.NotEmpty(t, output)
+	tests := []test{
+		// No Args
+		{args: []string{""}},
+		// Encode HMAC
+		{args: []string{"encode", "-a", "HS256", "-s", "secret"}},
+		{args: []string{"encode", "-a", "HS384", "-s", "secret"}},
+		{args: []string{"encode", "-a", "HS512", "-s", "secret"}},
+		// Encode RSA
+		{args: []string{"encode", "-a", "RS256", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		{args: []string{"encode", "-a", "RS256", "-s", "@./test/rsa_pkcs1_private.der"}},
+		{args: []string{"encode", "-a", "RS256", "-s", "@./test/rsa_pkcs8_private.pem"}},
+		{args: []string{"encode", "-a", "RS256", "-s", "@./test/rsa_pkcs8_private.der"}},
+		{args: []string{"encode", "-a", "RS384", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		{args: []string{"encode", "-a", "RS512", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		{args: []string{"encode", "-a", "PS256", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		{args: []string{"encode", "-a", "PS384", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		{args: []string{"encode", "-a", "PS512", "-s", "@./test/rsa_pkcs1_private.pem"}},
+		// Encode ECDSA
+		{args: []string{"encode", "-a", "ES256", "-s", "@./test/ecdsa_ec_private.pem"}},
+		{args: []string{"encode", "-a", "ES256", "-s", "@./test/ecdsa_ec_private.der"}},
+		{args: []string{"encode", "-a", "ES256", "-s", "@./test/ecdsa_pkcs8_private.pem"}},
+		{args: []string{"encode", "-a", "ES256", "-s", "@./test/ecdsa_pkcs8_private.der"}},
+		{args: []string{"encode", "-a", "ES384", "-s", "@./test/ecdsa_ec_private.pem"}},
+		{args: []string{"encode", "-a", "ES512", "-s", "@./test/ecdsa_ec_private.pem"}},
+		// Decode
+		{args: []string{"decode", "@./test/hmac_hs256.jwt"}},
+		// Validate HMAC
+		{args: []string{"validate", "@./test/hmac_hs256.jwt", "-a", "HS256", "-s", "secret"}},
+		{args: []string{"validate", "@./test/hmac_hs384.jwt", "-a", "HS384", "-s", "secret"}},
+		{args: []string{"validate", "@./test/hmac_hs512.jwt", "-a", "HS512", "-s", "secret"}},
+		// Validate RSA
+		{args: []string{"validate", "@./test/rsa_rs256.jwt", "-a", "RS256", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_rs256.jwt", "-a", "RS256", "-s", "@./test/rsa_pkcs1_public.der"}},
+		{args: []string{"validate", "@./test/rsa_rs256.jwt", "-a", "RS256", "-s", "@./test/rsa_x509_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_rs256.jwt", "-a", "RS256", "-s", "@./test/rsa_x509_public.der"}},
+		{args: []string{"validate", "@./test/rsa_rs384.jwt", "-a", "RS384", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_rs512.jwt", "-a", "RS512", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_ps256.jwt", "-a", "PS256", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_ps384.jwt", "-a", "PS384", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		{args: []string{"validate", "@./test/rsa_ps512.jwt", "-a", "PS512", "-s", "@./test/rsa_pkcs1_public.pem"}},
+		// Validate ECDSA
+		{args: []string{"validate", "@./test/ecdsa_es256.jwt", "-a", "ES256", "-s", "@./test/ecdsa_x509_public.pem"}},
+		{args: []string{"validate", "@./test/ecdsa_es256.jwt", "-a", "ES256", "-s", "@./test/ecdsa_x509_public.der"}},
+		{args: []string{"validate", "@./test/ecdsa_es384.jwt", "-a", "ES384", "-s", "@./test/ecdsa_x509_public.pem"}},
+		{args: []string{"validate", "@./test/ecdsa_es512.jwt", "-a", "ES512", "-s", "@./test/ecdsa_x509_public.pem"}},
+	}
+
+	for _, tt := range tests {
+		cmd := exec.Command("go", append([]string{"run", "jwt.go"}, tt.args...)...)
+		output, err := cmd.Output()
+
+		// Should have some output and not errored
+		assert.Nil(t, err)
+		assert.NotEmpty(t, output)
+	}
+
 }
--- test/_keys.sh
+++ test/_keys.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# hmac jwt
+go run ../jwt.go encode -a HS256 -s secret > hmac_hs256.jwt
+go run ../jwt.go encode -a HS384 -s secret > hmac_hs384.jwt
+go run ../jwt.go encode -a HS512 -s secret > hmac_hs512.jwt
+
 # rsa
 openssl genrsa -out rsa_pkcs1_private.pem 2048
 openssl rsa -in rsa_pkcs1_private.pem -outform DER -out rsa_pkcs1_private.der
@@ -10,6 +15,14 @@ openssl pkcs8 -topk8 -in rsa_pkcs1_private.pem -nocrypt -outform DER -out rsa_pk
 openssl rsa -in rsa_pkcs1_private.pem -outform PEM -pubout -out rsa_x509_public.pem
 openssl rsa -in rsa_pkcs1_private.pem -outform DER -pubout -out rsa_x509_public.der
 
+# rsa jwt
+go run ../jwt.go encode -a RS256 -s @./rsa_pkcs1_private.pem > rsa_rs256.jwt
+go run ../jwt.go encode -a RS384 -s @./rsa_pkcs1_private.pem > rsa_rs384.jwt
+go run ../jwt.go encode -a RS512 -s @./rsa_pkcs1_private.pem > rsa_rs512.jwt
+go run ../jwt.go encode -a PS256 -s @./rsa_pkcs1_private.pem > rsa_ps256.jwt
+go run ../jwt.go encode -a PS384 -s @./rsa_pkcs1_private.pem > rsa_ps384.jwt
+go run ../jwt.go encode -a PS512 -s @./rsa_pkcs1_private.pem > rsa_ps512.jwt
+
 # ecdsa
 openssl ecparam -name secp521r1 -genkey -noout -outform PEM -out ecdsa_ec_private.pem
 openssl ec -in ecdsa_ec_private.pem -outform PEM -out ecdsa_ec_private.der
@@ -17,6 +30,11 @@ openssl pkcs8 -topk8 -in ecdsa_ec_private.pem -nocrypt -outform PEM -out ecdsa_p
 openssl pkcs8 -topk8 -in ecdsa_ec_private.pem -nocrypt -outform DER -out ecdsa_pkcs8_private.der
 openssl ec -in ecdsa_ec_private.pem -outform PEM -pubout -out ecdsa_x509_public.pem
 openssl ec -in ecdsa_ec_private.pem -outform DER -pubout -out ecdsa_x509_public.der
+
+# ecdsa jwt
+go run ../jwt.go encode -a ES256 -s @./ecdsa_ec_private.pem > ecdsa_es256.jwt
+go run ../jwt.go encode -a ES384 -s @./ecdsa_ec_private.pem > ecdsa_es384.jwt
+go run ../jwt.go encode -a ES512 -s @./ecdsa_ec_private.pem > ecdsa_es512.jwt
 
 # openssh
 ssh-keygen -t rsa -b 2048 -N "" -C "" -f ./rsa_openssh_private
